5835bb4763d2616a4d26b371d256cc32e3e78487
Jana1902 <jana7cr@gmail.com>
2026-01-13 23:40:37 +0530

    Initial

--- server/src/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { registerUser } from '../controllers/userController.js';
-
-const router = express.Router();
-
-// This maps to POST /api/users
-router.post('/', registerUser);
-
-export default router;
